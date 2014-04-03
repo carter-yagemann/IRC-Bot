0a253834fba3ef83789884ee59777813bf92485c
Carter Yagemann <carter.yagemann@gmail.com>
2014-04-02 23:42:58 -0400

    Added license in preparation to open source

--- ircbot.cpp
+++ ircbot.cpp
@@ -1,8 +1,18 @@
 /*
- * ircbot.cpp
+ * IRC Bot - A simple C++ IRC library
+ * Copyright (C) 2014  Carter Yagemann
  *
- * Created..: 4/2/2014
- * By.......: Carter Yagemann
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ircbot.h"
